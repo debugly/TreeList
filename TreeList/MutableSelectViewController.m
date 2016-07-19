3baa0238030bdc2e587f0d69462f64c9a1489338
debugly <qianlongxu@gmial.com>
2016-07-20 00:21:28 +0800

    自定义编辑按钮，多选完成后删除cell；
    自定义编辑按钮，多选完成后删除cell；注释更加详细；

--- TreeList/MutableSelectViewController.m
+++ TreeList/MutableSelectViewController.m
@@ -24,21 +24,37 @@
     return _objects;
 }
 
+///自定义编辑按钮！
+- (UIBarButtonItem *)editButtonItem
+{
+    //根据当前的编辑状态返回标题
+    BOOL isEdit = self.isEditing;
+    NSString *title = isEdit ? @"完成" : @"编辑";
+    UIBarButtonItem *item = [[UIBarButtonItem alloc]initWithTitle:title style:UIBarButtonItemStylePlain target:self action:@selector(setEditing:animated:)];
+    return item;
+}
+
+- (void)updateEditBarButtonItem
+{
+    self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [self updateEditBarButtonItem];
     
     int random = arc4random() % 200 + 15;
+    int i = 0;
     while (random--) {
-        [self.objects addObject:[NSDate date]];
+        [self.objects addObject:@(i++)];
     }
   
 //正常情况下的选择，不影响编辑状态！
     /*
      是否允许选中；不会影响 allowsMultipleSelection；即使这个为NO，也可以多选！
      */
-    self.tableView.allowsSelection = NO;
+    self.tableView.allowsSelection = YES;
     
     /*
      是否允许正常情况下多选；允许时，可通过这个属性获取选择的cell；
@@ -94,6 +110,7 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
     NSDate *date = self.objects[indexPath.row];
     cell.textLabel.text = [date description];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"row:%ld",indexPath.row];
     return cell;
 }
 
@@ -122,8 +139,31 @@
 //重写这个方法，处理多选处理！
 - (void)setEditing:(BOOL)editing animated:(BOOL)animated
 {
-    [super setEditing:editing animated:animated];
     NSLog(@"---%d",self.tableView.allowsMultipleSelectionDuringEditing);
+    //重写了 editButtonItem 方法，因此这里需要取下反！
+    editing = !self.editing;
+    //结束编辑;
+    if (!editing) {
+        //获取选择行的indexpath；
+        NSArray *selectedRows = self.tableView.indexPathsForSelectedRows;
+        //因为我只有一个区，因此我可以偷懒（^_^）,直接获取row数组；对应的就是数组的下标！
+        NSArray *selectedIdx = [selectedRows valueForKeyPath:@"self.row"];
+        //很多人没用过这个方法，将arr转为indexset的；
+        NSIndexSet *indexset = [selectedIdx indexesOfObjectsPassingTest:^BOOL(NSNumber * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            return YES;
+        }];
+        //有选择就删除；这里可以根据自己的业务逻辑写；
+        if (indexset && indexset.count > 0) {
+            [self.objects removeObjectsAtIndexes:indexset];
+            [self.tableView beginUpdates];
+            [self.tableView deleteRowsAtIndexPaths:selectedRows withRowAnimation:UITableViewRowAnimationAutomatic];
+            [self.tableView endUpdates];
+        }
+    }
+    //先处理业务，然后再调用super，因为调用super会清空 indexPathsForSelectedRows ！！！
+    [super setEditing:editing animated:animated];
+    //更新下；
+    [self updateEditBarButtonItem];
 }
 
 - (void)printfIndexPath:(NSIndexPath *)idx optPrefix:(NSString *)prefix
@@ -131,12 +171,18 @@
     NSLog(@"%@idx<%p>:%ld-%ld",prefix?prefix:@"",idx,(long)idx.section,(long)idx.row);
 }
 
+///编辑态、正常态 点击选择均会回调哦！
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    [self printfIndexPath:indexPath optPrefix:@"当次选择："];
-    [self printfIndexPath:tableView.indexPathForSelectedRow optPrefix:@"SelectedRow："];
-    for (NSIndexPath *idx in tableView.indexPathsForSelectedRows) {
-        [self printfIndexPath:idx optPrefix:@"SelectedRows:"];
+    if(tableView.isEditing){
+        [self printfIndexPath:indexPath optPrefix:@"当次选择："];
+        [self printfIndexPath:tableView.indexPathForSelectedRow optPrefix:@"SelectedRow："];
+        for (NSIndexPath *idx in tableView.indexPathsForSelectedRows) {
+            [self printfIndexPath:idx optPrefix:@"SelectedRows:"];
+        }
+    }else{
+        ///自动取消选择
+        [tableView deselectRowAtIndexPath:indexPath animated:YES];
     }
 }
 
